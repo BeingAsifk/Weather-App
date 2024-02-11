b650c44502bf078cad4bfe332abb7cbd85438a4b
Asif Khan <beingasifk07@gmail.com>
2024-02-11 17:19:11 +0530

    first commit

---
+++ src/components/SearchMain.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import "../components/style.css";
+import WeatherDetails from "./WeatherDetails";
+
+function SearchMain() {
+  const [searchTerm, setSearchTerm] = useState("delhi");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=f840f08444c59ce09a2fbb87f9bff214`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+      const { temp, humidity, pressure } = data.main;
+      const { main: weatherType } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weatherType,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search-container">
+          <input
+            className="search-input"
+            type="search"
+            placeholder="type city name..."
+            id="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="search-btn" onClick={getWeatherInfo}>
+            <img
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Objects/Magnifying%20Glass%20Tilted%20Left.webp"
+              alt="Magnifying Glass Tilted Left"
+              width="30"
+              height="30"
+            />
+          </button>
+        </div>
+      </div>
+
+      <WeatherDetails {...tempInfo} />
+    </>
+  );
+}
+
+export default SearchMain;
